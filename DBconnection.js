a88086e3fb9c5121de11d5bd01f0989d70ff2cfa
archi <archismandaddy1996@gmail.com>
2024-10-14 22:17:18 +0530

    DB Connection Password

--- DBconnection.js
+++ DBconnection.js
@@ -1,9 +1,9 @@
 const { Sequelize } = require('sequelize');
-
+require('dotenv').config();
 // Option 1: Passing a connection URI
 
 // Option 3: Passing parameters separately (other dialects)
-const db_connection = new Sequelize('home_project', 'root', 'arijit', {
+const db_connection = new Sequelize('home_project', 'root', process.env.SECRET, {
   host: 'localhost',
   dialect: 'mysql',
   port:3306,  
